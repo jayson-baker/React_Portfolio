b84195b50238347b5b1120003c8af1145ce3312a
Jayson Baker <bakej07@gmail.com>
2023-07-26 16:33:13 -0500

    Functional hambuerger for mobile view

--- react_portfolio/src/components/Navbar.jsx
+++ react_portfolio/src/components/Navbar.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import React, {useState} from 'react'
 import {FaBars, FaTimes} from 'react-icons/fa'
 import Logo from '../assets/logo.png'
 
 const Navbar = () => {
+    const [nav, setNav] = useState(false)
+    const handleClick = () => setNav(!nav)
   return (
     <div className='fixed w-full h-[80px] flex justify-between items-center px-4 bg-[#0A192f] text-gray-300'>
         <div>
@@ -19,16 +21,16 @@ const Navbar = () => {
             </ul>
 
         {/* Hamburger */}
-        <div className='hidden'>
+        <div onClick={handleClick} className='md:hidden z-10'>
             <FaBars />
         </div>
         {/* Mobile Menu */}
-            <ul className='hidden'>
-                <li>Home</li>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Work</li>
-                <li>Contact</li>
+            <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0A192f] flex flex-col justify-center items-center'}>
+                <li className='py-6 text-4xl'>Home</li>
+                <li className='py-6 text-4xl'>About</li>
+                <li className='py-6 text-4xl'>Skills</li>
+                <li className='py-6 text-4xl'>Work</li>
+                <li className='py-6 text-4xl'>Contact</li>
             </ul>
 
         {/* Social Icons */}
