82b37e8edc3e7e997361c280ee7b52577bc534cb
Jayson Baker <bakej07@gmail.com>
2023-07-27 13:13:24 -0500

    Text color fix again

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -6,7 +6,7 @@ const Contact = () => {
         <form method='POST' action="https://getform.io/f/02528340-9e3b-4f17-aa54-26b7ee0bee92" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-[#e69b3a] text-gray-300'>Contact</p>
-                <p className=' text-gray-300 py-4'>Submit form below or email me at bakej07@gmail.com</p>
+                <p className='text-white py-4'>Submit form below or email me at bakej07@gmail.com</p>
             </div>
             <input className='bg-gray-300 p-2' type="text" placeholder='Name' name='name' />
             <input className='my-4 p-2 bg-gray-300' type="email" placeholder='Email' name='email' />
